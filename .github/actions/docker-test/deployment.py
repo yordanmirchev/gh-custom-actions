5f2cfd05858ebfb64d4695aa8e6622ab4fa60a38
Yordan Mirchev <ymm@example.com>
2023-05-03 22:17:16 +0300

    Add completed project. Add custom action.

---
+++ .github/actions/docker-test/deployment.py
@@ -0,0 +1,18 @@
+import os
+def run():
+    user_input = os.environ['INPUT_USER-INPUT']
+    # user_input = "foo"
+
+    # As a next step we can save this as an output.
+    # print(os.environ)
+
+    result = "in: " + user_input + "\nenv: " + str(os.environ)
+
+    # The below code sets the 'website-url' output (the old ::set-output syntax isn't supported anymore - that's the only thing that changed though)
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as gh_output:
+        print(f'action_result={result}', file=gh_output)
+
+    print(f'result is: \n{result}')
+
+if __name__ == '__main__':
+    run()
